a92f390fd0750872654f4d2c6ea25240ec093640
Toon Verwerft <toonverwerft@gmail.com>
2020-08-12 13:44:14 +0200

    call in subroutine

--- process.php
+++ process.php
@@ -4,8 +4,7 @@ if (!$url ?? null) {
     throw new \Exception('no url ...');
 }
 
-return new \Amp\Success($url);
-
-
-//$process = new Symfony\Component\Process\Process(['php', '-r', 'sleep(30); echo "'.$url.'";']);
-//echo $process->mustRun()->getOutput();
+return \Amp\call(function () use ($url) {
+    $process = new Symfony\Component\Process\Process(['php', '-r', 'echo "'.$url.'";']);
+    return $process->mustRun()->getOutput();
+});
