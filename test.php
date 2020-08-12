44541d64eb8e9200cc9aaf4827c6e7432628d571
Toon Verwerft <toonverwerft@gmail.com>
2020-08-12 14:01:46 +0200

    Try cancelable

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/MultiPromise.php';
 
-use function Amp\ParallelFunctions\parallelMap;
+use Amp\LazyPromise;
+use Amp\Parallel\Worker\DefaultPool;
+use function Amp\ParallelFunctions\parallel;
 use function Amp\Promise\wait;
 
 function printErrors(\Throwable $e) {
@@ -17,18 +20,45 @@ function printErrors(\Throwable $e) {
 
 
 try {
+    $pool = new DefaultPool(32);
     $responses = wait(
-        parallelMap(
-            [
-                'https://google.com/',
-                'https://github.com/',
-                'https://stackoverflow.com/',
-            ],
-            function ($url) {
-                return wait(require 'process.php');
-            },
-            new \Amp\Parallel\Worker\DefaultPool(32)
-        )
+        \Amp\call(function () {
+            [$errors, $results] = yield MultiPromise::cancelable(
+                [
+                    new LazyPromise(function () {
+                        return parallel(function () {
+                            $url = 'https://google.com/';
+                            return require 'process.php';
+                        })();
+                    }),
+                    new LazyPromise(function () {
+                        return parallel(function () {
+                            $url = 'https://github.com/';
+                            return require 'process.php';
+                        })();
+                    }),
+                    new LazyPromise(function () {
+                        return parallel(function () {
+                            $url = 'https://stackoverflow.com/';
+                            return require 'process.php';
+                        })();
+                    }),
+                ],
+                function () {
+                    return false;
+                }
+            );
+
+            if (count($errors)) {
+
+                foreach ($errors as $error) {
+                    printErrors($error);
+                }
+                exit(1);
+            }
+
+            return $results;
+        })
     );
 
     var_dump($responses);
